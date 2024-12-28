ce8e684f42260ffdfd57410d8387eb7866536484
Gabriel Husain <gabe-husain@gmail.com>
2024-12-27 23:30:28 -0500

    More css for cleaner website and division

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,15 @@ import TextPage from "@/src/components/layout/Textpage";
 export default function About() {
     return (
       <TextPage>
-        <h1 className="text-4xl font-bold">About Page</h1>
+        <div className="wrapper multiline">
+        <title>About Page</title>
+
+        <h1 className="title">About Page</h1>
+
+        <p className="textBody">This is Gabriel's website. 
+          He likes to code and cook and make cool things, or at least things that he thinks are cool. 
+          not much though...</p>
+          </div>
       </TextPage>
     )
   }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,15 @@ import TextPage from "../components/layout/Textpage";
 
 export default function Home() {
   return (
-    <TextPage>
+    <TextPage >
+      <div className="wrapper multiline">
       <title>Welcome to Gabriel's Website</title>
+      <h1 className="title">Welcome to Gabriel's Website </h1>
 
-      <h1>There's not much here. Not even a couple pages.</h1>
+      <p className="textBody">There's not much here. Not even a couple pages.
 
+      </p>
+      </div>
     </TextPage>
   )
 }
