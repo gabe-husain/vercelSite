e13765bcc6c66e2692d898fb9e4863f9f4c35b13
Gabriel Husain <gabe-husain@gmail.com>
2025-01-09 05:27:21 -0500

    darkmode and twitter

--- src/components/layout/MarkdownRenderer.tsx
+++ src/components/layout/MarkdownRenderer.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import "@/src/styles/Markdown.css";
+import rehypeRaw from "rehype-raw";
 
 export default function MarkdownRenderer({ children }: { children: string }) {
   return (
   <div className='base'>
-    <ReactMarkdown>{children}</ReactMarkdown>
+    <ReactMarkdown rehypePlugins={[rehypeRaw]}>{children}</ReactMarkdown>
   </div>
   );
 }
